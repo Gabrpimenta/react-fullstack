222ff97bcb5bd5abad2c2fae2c7fee12e5cfe8e2
Gabrpimenta <gabrielrpimenta@gmail.com>
2022-09-05 10:42:02 -0300

    ✨ login and registration

--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");
+const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
-  const post = req.body;
-  await Posts.create(post);
-  res.json(post);
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json({ message: "User created" });
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await Users.findOne({ where: { username: username } });
+  if (!user) {
+    res.json({ error: "User doesn't exist" });
+  }
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) res.json({ error: "Wrong username/password combination" });
+    res.json({ message: "Logged in!" });
+  });
 });
 
 module.exports = router;
